4291a70e61e2e892bb3dadfc0e8703e85cd337fb
Jared <hoffmaj2@hoffmaj2-ThinkPad-W530.(none)>
2016-02-20 07:12:22 -0500

    fixed front end again

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,1 +1,13 @@
 var app = angular.module('neuropolis');
+
+app.service('NeuronMaker', ['$http', function($http){
+        this.getConvertedData = function(path, callback){
+        $http({method: 'GET', url: path})
+            .then(function(response){
+                callback(response.data);
+            }, function(response){
+                console.log('Failure');
+                console.log(response);
+            });
+        };
+    }]);
