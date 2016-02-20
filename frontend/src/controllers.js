15feef3fb82135a358c6c0626f46409d84d63e8e
Jared <hoffmaj2@hoffmaj2-ThinkPad-W530.(none)>
2016-02-20 15:10:00 -0500

    added training service to web client

--- frontend/src/controllers.js
+++ frontend/src/controllers.js
@@ -1,6 +1,10 @@
 var app = angular.module('neuropolis');
 
-app.controller('NeuralNetController',['NeuralNet', function(NeuralNet){
+app.controller('NeuralNetController', ['NeuralNet', function(NeuralNet){
+	//this.net = NeuralNet.myNet;
+	NeuralNet.makeNetwork(5,2,1);
+	Inputs = [true,true];
+	Outputs = [true];
+	NeuralNet.trainWeights(25,NeuralNet.myNet,Inputs,Outputs);
 	this.net = NeuralNet.myNet;
-	NeuralNet.makeNetwork(4,2,2);
 }]);
--- frontend/src/services.js
+++ frontend/src/services.js
@@ -21,20 +21,22 @@ app.service('NeuralNet', ['$http',
 
 			for (var i = 0; i < nodes; i++) {
 				for (var j = 0; j < nodes; j++) {
+					var toadd = Math.random();
 					nnet.connections.push({
 						"to": i,
 						"from": j,
-						"weight": 0.0
+						"weight": toadd
 					});
 				}
 			}
 
 			for (var i = 0; i < nodes; i++) {
 				for (var j = 0; j < sensors; j++) {
+					var toadd = Math.random();
 					nnet.inputs.push({
 						"to": i,
 						"from": j,
-						"weight": 0.0
+						"weight": toadd
 					});
 				}
 			}
@@ -44,14 +46,85 @@ app.service('NeuralNet', ['$http',
 			}
 
 			for (var i = 0; i < nodes; i++) {
-				nnet.thresholds.push(0);
+				var toadd = Math.random();
+				nnet.thresholds.push(toadd);
 			}
 
-			myNet = nnet;
+			for (var i = 0; i < sensors; i++) {
+				//var toadd = Math.random();
+				nnet.sensor_thresholds.push(0.5);
+			}
+
+
+			this.myNet = nnet;
 			return nnet;
 		}
 
 		this.myNet = {};
+		this.state = {};
+		this.bools = [];
+		this.nextBools = [];
+
+		this.synergy = 0.3;
+		this.degeneration = 0.0;
+		//this.sbools = [];
+
+		this.trainWeights = function(steps, net, inps, ops){
+			for (var i = 0; i < steps; i++) {
+				this.synergyStep(net,inps, ops);
+			}
+		};
+
+		this.synergyStep = function(net, inps, ops) {
+			this.updateNetwork(net,inps);
+			for (var i = 0; i < net.connections.length; i++) {
+				if(net.connections[i].to != net.connections[i].from){
+					if(this.bools[net.connections[i].to] && this.bools[net.connections[i].from]){
+						net.connections[i].weight += this.synergy;
+					}
+					else{
+						net.connections[i].weight = net.connections[i].weight * this.degeneration;
+					}
+
+					for (var j = 0; j < net.outputs.length; j++) {
+						if (this.bools[net.outputs[j]] && this.bools[net.connections[i].from]) {
+							net.connections[i].weight += this.synergy;
+						}
+						if ((!this.bools[net.outputs[j]]) && this.bools[net.connections[i].from]) {
+							net.connections[i].weight -= this.synergy;
+						}
+					}
+				}
+			}
+		};
+
+		this.updateNetwork = function(net,inps) {
+			for (var i = 0; i < net.nodes; i++) {
+				this.checkNeuron(net,i,inps);
+			}
+			for (var i = 0; i < net.nodes; i++) {
+				this.bools[i] = this.nextBools[i];
+			}
+		};
+
+		this.checkNeuron = function(net, n, inps) {
+			var vl = vl += net.connections.weight;
+			0.0;
+			vl += net.connections.weight;
+			for (var i = 0; i < net.connections.length; i++) {
+				if (net.connections[i].to == n && this.bools[net.connections[i].from]) {
+					vl += net.connections[i].weight;
+				}
+			}
+			for (var i = 0; i < net.inputs.length; i++) {
+				if (net.inputs[i].to == n && this.inps[net.connections[i].from]) {
+					vl += net.inputs[i].weight;
+				}
+			}
+			if (vl > net.thresholds[n]) {this.nextBools[n] = true; return true;}
+			this.nextBools[n] = false;
+			return false;
+		};
 
 	}
 ]);
