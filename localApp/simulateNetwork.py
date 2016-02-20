b6567894cdbe84be599e68df9ba87d619714295c
bonattt <bonattt@rose-hulman.edu>
2016-02-20 14:49:54 -0500

    asdfghjkl

--- localApp/simulateNetwork.py
+++ localApp/simulateNetwork.py
@@ -1,19 +1,36 @@
-import exceptions
 import json
-import httplib
+import urllib2
+import sys
 
 
-def main(): 
-	neuralNet = load_neural_net('../testData/andNet.json')
-	# process_neural_net(neuralNet)
+def main():
+	if len(sys.argv) < 2:
+		exit("incorrect number of arguments")
+	neural_net_file = sys.argv[0]
+	config_file = sys.argv[1]
+	neuralNet = load_neural_net(neural_net_file, config_file)
+	process_neural_net(neuralNet)
+	output_array = neuralNet.getOutput()
 
-def load_neural_net(json_path):
+	with open(config_file) as data_file:
+		data = json.load(data_file)
+		output_url = data['output_url']
+		deliver_outpout(output_array, output_url)
+
+def deliver_outpout(output_array, output_url):
+	data = {"output": output_array}
+	request = urllib2.Request("http://" + output_url)
+	request.add_header('neural-net output', 'application-json')
+	urllib2.urlopen(request, json.dumps(data))
+
+
+def load_neural_net(json_path, json_config):
 	with open(json_path) as data_file:
 		data = json.load(data_file)
 		
 		connections = data["connections"]
 		
-		sensor_ports = data["sensors"]
+		num_sensors = data["sensors"]
 		inputs = data["inputs"]
 
 		num_neurons = data["num_neurons"]
@@ -22,20 +39,26 @@ def load_neural_net(json_path):
 		sensor_thresholds = data["sensor_thresholds"]
 		remainingRuns = data["timesteps"]
 
-		# initialize neurons
-		neurons = []
-		for k in range(num_neurons):
-			neurons.append(Neuron(thresholds[k]))
+	with open(json_config) as data_file:
+		data = json.load(data_file)
+		sensor_urls = data['sensor_urls']
 
-		# add connections
-		for connection in connections:
-			parse_neuron_connection(connection, neurons)
+	# initialize neurons
+	neurons = []
+	for k in range(num_neurons):
+		neurons.append(Neuron(thresholds[k]))
+	# add connections
+	for connection in connections:
+		parse_neuron_connection(connection, neurons)
 
-		for k in range(len(sensor_ports)):
-			port = sensor_ports[k]
-			threshold = sensor_thresholds[k]
-			new_sensor = Sensor(threshold, port)
-			neurons.append(new_sensor)
+	sensors = []
+	for k in range(num_sensors):
+		url = sensor_urls[k]
+		threshold = sensor_thresholds[k]
+		new_sensor = Sensor(threshold, url)
+		sensors.append(new_sensor)
+	for inputPath in inputs:
+		neurons[inputPath["to"]].inputs[sensors[inputPath["from"]]] = inputPath["weight"]
 
 	return NeuralNet(neurons, outputs, remainingRuns)
 
@@ -44,7 +67,7 @@ def parse_neuron_connection(connection, neurons):
 	"""parsses the given connection, and addes it to the connections map of the correct neuron"""
 	neuron = neurons[connection["to"]]
 	neuron.inputs[neurons[connection["from"]]] = connection["weight"]
-	return neuron	
+	return neuron
 
 def process_neural_net(neuralNet):
 	while(neuralNet.remainingRuns > 0):
@@ -63,36 +86,28 @@ class NeuralNet():
 			neuron.setNextState()
 		for neuron in self.neurons:
 			neuron.update()
-		remainingRuns -= 1
+		self.remainingRuns -= 1
 
 	def getOutput(self):
 		finalOut = []
 		for i in self.outputs:
-			finalOut.append(self.neurons[i])
+			finalOut.append(self.neurons[i].isActive)
 		return finalOut
 
 
 class Sensor():
 
-	def __init__(self, threshold, port):
-		self.value = 0
-		self.port = port
+	def __init__(self, threshold, url):
+		response = urllib2.urlopen('http://' + url)
+		self.value = float(response.read())
 		self.threshold = threshold
-		self.isActive = False
-		self.nextActive = False 
+		self.isActive = self.value > self.threshold
 
 	def setNextState(self):
-
-		conn = httplib.HTTPConnection('http://' + self.port) # TODO
-		conn.request("HEAD", "/index.html")
-		result = conn.getResponse()
-		print(result)
+		pass
 
 	def update(self): 
-		if self.nextActive == None:
-			raise exceptions.NextStateNullException()
-		self.isActive = self.nextActive
-		self.nextActive = None
+		pass
 
 
 class Neuron():
@@ -108,20 +123,32 @@ class Neuron():
 		return sum(ls)
 
 	def setNextState(self):
-		inputs = []
+		activeInputs = []
 		for neuron in self.inputs:
 			if neuron.isActive:
-				inputs.append(value)
-		if self.aggregate(inputs) > self.threshold:
+				value = self.inputs[neuron]
+				activeInputs.append(value)
+		if self.aggregate(activeInputs) > self.threshold:
 			self.nextActive = True
 		else:
 			self.nextActive = False
 
 	def update(self):
 		if self.nextActive == None:
-			raise exceptions.NextStateNullException()
+			raise NextStateIsNullException()
 		self.isActive = self.nextActive
 		self.nextActive = None
+
+
+class NextStateIsNullException(Exception):
+
+	def __init__(self):
+		pass
+
+class ConfigFileConflictingDataException(Exception):
+
+	def __init__(self):
+		pass
 
 
 if __name__ == "__main__":
--- localApp/testAndNet.py
+++ localApp/testAndNet.py
@@ -2,61 +2,11 @@ import simulateNetwork as sim
 
 
 def test():
-	result = test00()
-	result = result & test01()
-	result = result & test10()
-	result = result & test11()
-
-	if result:
-		print('all tests passed!')
-	else:
-		print('test cases failed')
-
-
-def test00():
-	neural_net = sim.load_neural_net('../testData/andNet.json')
+	print "testing And-gate neural net"
+	neural_net = sim.load_neural_net('../testData/andNet.json', '../testData/andNetCFG.json')
 	# MOCK Inputs
 	sim.process_neural_net(neural_net)
-	if neural_net.getOutput():
-		print('0 & 0 returned 1')
-		print('test00 FAILED')
-		return False
-	return True
-
-
-def test01():
-	neural_net = sim.load_neural_net('../testData/andNet.json')
-	# MOCK Inputs
-	sim.process_neural_net(neural_net)
-	if neural_net.getOutput():
-		print('0 & 1 returned 1')
-		print('test01 FAILED')
-		return False
-	return True
-
-
-def test10():
-	neural_net = sim.load_neural_net('../testData/andNet.json')
-	# MOCK Inputs
-	sim.process_neural_net(neural_net)
-	if neural_net.getOutput():
-		print('1 & 0 returned 1')
-		print('test10 FAILED')
-		return False
-	return True
-
-
-
-def test11():
-	neural_net = sim.load_neural_net('../testData/andNet.json')
-	# MOCK Inputs
-	sim.process_neural_net(neural_net)
-	if not neural_net.getOutput():
-		print('1 & 1 returned 0')
-		print('test11 FAILED')
-		return False
-	return True
-
+	print neural_net.getOutput()
 
 if __name__ == "__main__":
 	test()
