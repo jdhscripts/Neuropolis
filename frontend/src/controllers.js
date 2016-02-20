193fb3780b2d103eeb976a3ce433dfd969c3e3f6
Jared <hoffmaj2@hoffmaj2-ThinkPad-W530.(none)>
2016-02-20 08:36:56 -0500

    stuff

--- frontend/src/controllers.js
+++ frontend/src/controllers.js
@@ -1,1 +1,6 @@
 var app = angular.module('neuropolis');
+
+app.controller('NeuralNetController',['NeuralNet', function(NeuralNet){
+	this.net = NeuralNet.myNet;
+	NeuralNet.makeNetwork(4,2,2);
+}]);
