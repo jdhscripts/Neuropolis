88d57e77cec7b54ae2da60eb0214e547559e1d2d
Jared <hoffmaj2@hoffmaj2-ThinkPad-W530.(none)>
2016-02-20 19:06:42 -0500

    change network tests

--- frontend/src/controllers.js
+++ frontend/src/controllers.js
@@ -2,16 +2,16 @@ var app = angular.module('neuropolis');
 
 app.controller('NeuralNetController', ['NeuralNet', function(NeuralNet){
 	//this.net = NeuralNet.myNet;
-	NeuralNet.makeNetwork(5,2,1);
+	NeuralNet.makeNetwork(20,2,1);
 	//Inputs = [true,true];
 	//Outputs = [true];
 	tests = [
 	{inps : [true, true], ops : [true]},
-	{inps : [true, false], ops : [true]},
-	{inps : [false, true], ops : [true]},
+	{inps : [true, false], ops : [false]},
+	{inps : [false, true], ops : [false]},
 	{inps : [false, false], ops : [false]}
 	];
-	NeuralNet.aggregateWeightLearning(5,NeuralNet.myNet,tests);
+	NeuralNet.aggregateWeightLearning(400,NeuralNet.myNet,tests);
 	this.net = NeuralNet.myNet;
 	this.net.connections = this.net.connections;//.filter(function(el){true});
 }]);
